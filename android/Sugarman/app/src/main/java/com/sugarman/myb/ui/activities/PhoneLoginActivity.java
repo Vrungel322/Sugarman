fd70b298945402f9f211e9caaa42df109157834e
Nikita <nikita241296@gmail.com>
2017-10-23 15:24:27 +0300

    scroll spinner in PhoneLoginActivity to current country

--- android/Sugarman/app/src/main/java/com/sugarman/myb/ui/activities/PhoneLoginActivity.java
+++ android/Sugarman/app/src/main/java/com/sugarman/myb/ui/activities/PhoneLoginActivity.java
@@ -1,7 +1,9 @@
 package com.sugarman.myb.ui.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +14,7 @@ import butterknife.BindView;
 import butterknife.OnClick;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.sugarman.myb.App;
 import com.sugarman.myb.R;
 import com.sugarman.myb.api.models.responses.users.UsersResponse;
 import com.sugarman.myb.listeners.ApiRefreshUserDataListener;
@@ -21,6 +24,7 @@ import com.sugarman.myb.utils.Converters;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -90,6 +94,12 @@ public class PhoneLoginActivity extends GetUserInfoActivity implements ApiRefres
   }
 
   private void setUpSpinner() {
+    TelephonyManager manager =
+        (TelephonyManager) App.getInstance().getSystemService(Context.TELEPHONY_SERVICE);
+    String currentCountry = new Locale("",manager.getSimCountryIso().toUpperCase()).getDisplayCountry();
+    int currentCountryPosition = 0;
+
+
     String myJson = Converters.loadAssetTextAsString(getBaseContext(), "countryCode.json");
     Type listType = new TypeToken<List<CountryCodeEntity>>() {
     }.getType();
@@ -97,11 +107,15 @@ public class PhoneLoginActivity extends GetUserInfoActivity implements ApiRefres
     List<String> items = new ArrayList<>();
     for (int i = 0; i < mCountryCodeEntities.size(); i++) {
       items.add(mCountryCodeEntities.get(i).getCountryName());
+      if (mCountryCodeEntities.get(i).getCountryName().contains(currentCountry)){
+        currentCountryPosition = i;
+      }
     }
     ArrayAdapter<String> adapter =
         new ArrayAdapter<String>(this, R.layout.item_country_code, items);
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     mSpinnerCountryCode.setAdapter(adapter);
+    mSpinnerCountryCode.setSelection(currentCountryPosition);
     etPhoneNumber.setText(mCountryCodeEntities.get(0).getCode());
   }
 
