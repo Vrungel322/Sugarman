f0040098d3498ae4bee7050a7ca6ea5a491d0c53
Nikita <nikita241296@gmail.com>
2017-10-27 15:04:41 +0300

    add fetch mentors method to presenter

--- android/Sugarman/app/src/main/java/com/sugarman/myb/ui/activities/mentorList/MentorListActivityPresenter.java
+++ android/Sugarman/app/src/main/java/com/sugarman/myb/ui/activities/mentorList/MentorListActivityPresenter.java
@@ -1,14 +1,31 @@
 package com.sugarman.myb.ui.activities.mentorList;
 
+import com.arellomobile.mvp.InjectViewState;
 import com.sugarman.myb.App;
 import com.sugarman.myb.base.BasicPresenter;
+import com.sugarman.myb.utils.ThreadSchedulers;
+import rx.Subscription;
 
 /**
  * Created by yegoryeriomin on 10/27/17.
  */
-
-public class MentorListActivityPresenter extends BasicPresenter<IMentorListActivityView> {
+@InjectViewState public class MentorListActivityPresenter
+    extends BasicPresenter<IMentorListActivityView> {
   @Override protected void inject() {
     App.getAppComponent().inject(this);
+  }
+
+  @Override protected void onFirstViewAttach() {
+    super.onFirstViewAttach();
+    fetchMentors();
+  }
+
+  private void fetchMentors() {
+    Subscription subscription = mDataManager.fetchMentors()
+        .compose(ThreadSchedulers.applySchedulers())
+        .subscribe(mentorEntities -> {
+          getViewState().fillMentorsList(mentorEntities);
+        });
+    addToUnsubscription(subscription);
   }
 }
