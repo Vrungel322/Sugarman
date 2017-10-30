1d5f62eaa1f6cc63e4925edd020a7208483470db
Yegor Yeriomin <yegor.yeriomin@gmail.com>
2017-10-30 12:21:54 +0200

    added toolbar for mentor list

---
+++ android/Sugarman/app/src/main/java/com/sugarman/myb/ui/activities/mentorDetail/MentorDetailActivity.java
@@ -0,0 +1,13 @@
+package com.sugarman.myb.ui.activities.mentorDetail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.sugarman.myb.R;
+
+public class MentorDetailActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_mentor_detail);
+  }
+}
