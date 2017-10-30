04f8e62e2aa7a43a2f6fb0d31dd5c548c884d688
Nikita <nikita241296@gmail.com>
2017-10-30 12:28:33 +0200

    mentors entity become parcelable

--- android/Sugarman/app/src/main/java/com/sugarman/myb/models/mentor/MentorEntity.java
+++ android/Sugarman/app/src/main/java/com/sugarman/myb/models/mentor/MentorEntity.java
@@ -1,5 +1,7 @@
 package com.sugarman.myb.models.mentor;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
@@ -7,15 +9,24 @@ import java.util.List;
  * Created by nikita on 27.10.2017.
  */
 
-public class MentorEntity {
-  @SerializedName("id")private String mentorId;
+public class MentorEntity implements Parcelable {
+  public static final Creator<MentorEntity> CREATOR = new Creator<MentorEntity>() {
+    @Override public MentorEntity createFromParcel(Parcel in) {
+      return new MentorEntity(in);
+    }
+
+    @Override public MentorEntity[] newArray(int size) {
+      return new MentorEntity[size];
+    }
+  };
+  @SerializedName("id") private String mentorId;
   @SerializedName("picture_url") private String mentorImgUrl;
-  @SerializedName("name")private String mentorName;
-  @SerializedName("rating")private String mentorRating;
+  @SerializedName("name") private String mentorName;
+  @SerializedName("rating") private String mentorRating;
   @SerializedName("mentor_num_comments") private String mentorNumComments;
-  @SerializedName("description")private String mentorDescription;
-  @SerializedName("skills")private List<MentorsSkills> mentorSkills;
-  @SerializedName("members_statuses")private List<MemberOfMentorsGroup> membersOfMentorsGroup;
+  @SerializedName("description") private String mentorDescription;
+  @SerializedName("skills") private List<MentorsSkills> mentorSkills;
+  @SerializedName("members_statuses") private List<MemberOfMentorsGroup> membersOfMentorsGroup;
 
   public MentorEntity(String mentorId, String mentorImgUrl, String mentorName, String mentorRating,
       String mentorNumComments, String mentorDescription, List<MentorsSkills> mentorSkills,
@@ -28,6 +39,32 @@ public class MentorEntity {
     this.mentorDescription = mentorDescription;
     this.mentorSkills = mentorSkills;
     this.membersOfMentorsGroup = membersOfMentorsGroup;
+  }
+
+  protected MentorEntity(Parcel in) {
+    mentorId = in.readString();
+    mentorImgUrl = in.readString();
+    mentorName = in.readString();
+    mentorRating = in.readString();
+    mentorNumComments = in.readString();
+    mentorDescription = in.readString();
+    mentorSkills = in.createTypedArrayList(MentorsSkills.CREATOR);
+    membersOfMentorsGroup = in.createTypedArrayList(MemberOfMentorsGroup.CREATOR);
+  }
+
+  @Override public void writeToParcel(Parcel dest, int flags) {
+    dest.writeString(mentorId);
+    dest.writeString(mentorImgUrl);
+    dest.writeString(mentorName);
+    dest.writeString(mentorRating);
+    dest.writeString(mentorNumComments);
+    dest.writeString(mentorDescription);
+    dest.writeTypedList(mentorSkills);
+    dest.writeTypedList(membersOfMentorsGroup);
+  }
+
+  @Override public int describeContents() {
+    return 0;
   }
 
   public String getMentorId() {
