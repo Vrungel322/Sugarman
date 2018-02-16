dedbe4143bf6a8853cec39dc06df7935328e87a6
Nikita <nikita241296@gmail.com>
2018-02-16 16:33:18 +0200

    listener add

---
+++ android/Sugarman/app/src/main/java/com/sugarman/myb/ui/activities/mentorDetail/GooglePurchaseListener.java
@@ -0,0 +1,11 @@
+package com.sugarman.myb.ui.activities.mentorDetail;
+
+import com.sugarman.myb.utils.purchase.PurchaseTransaction;
+
+/**
+ * Created by nikita on 16.02.2018.
+ */
+
+public interface GooglePurchaseListener {
+  void action(PurchaseTransaction purchaseTransaction);
+}
